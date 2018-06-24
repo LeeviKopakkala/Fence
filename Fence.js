ce3e8f53b73a3c5bd14b27c0432c0916065b9f51
Leevi Kopakkala <leevi.kopakkala@hotmail.com>
2018-06-24 22:50:14 +0300

    Add helpers.js

--- Fence.js
+++ Fence.js
@@ -7,13 +7,14 @@ config = require("./config.json");
 
 // Startup function
 client.on("ready", () => {
-  console.log('Bot has started'); 
-  client.user.setActivity(`with Node.JS`);
+  console.log('Fence has started'); 
+  client.user.setActivity(`Fence - A Discord Bot Framework`);
 });
 
 // Commands
 require("./src/commands/admin.js");
 require("./src/commands/basic.js");
+require("./src/commands/helpers.js");
 
 client.login(config.token);
            
